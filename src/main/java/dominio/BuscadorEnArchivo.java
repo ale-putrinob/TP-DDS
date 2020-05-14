57d1d781a1239cca10dfd7dcd6be286a752e37e0
AlePutrinoB <alejandroputrinob@hotmail.com>
2020-05-14 11:30:47 -0300

    Saco excepciones chequeadas

--- src/main/java/dominio/BuscadorEnArchivo.java
+++ src/main/java/dominio/BuscadorEnArchivo.java
@@ -10,26 +10,26 @@ import java.util.stream.Stream;
 
 public class BuscadorEnArchivo {
     
-    public static void encontrarIgualdad(String palabraBuscada, String direccionDelArchivo) throws IOException {
+    public static void encontrarIgualdad(String palabraBuscada, String direccionDelArchivo) {
     	Condicion condicion = (value, value2) -> value.equals(value2);
     	
     	encontrar(palabraBuscada, direccionDelArchivo, condicion);
     }
 
-	public static void encontrarInclusion(String palabraBuscada, String direccionDelArchivo) throws IOException {
+	public static void encontrarInclusion(String palabraBuscada, String direccionDelArchivo) {
 		Condicion condicion = (value, value2) -> value.contains(value2);
 		
 		encontrar(palabraBuscada, direccionDelArchivo, condicion);
 	}
 	
-	private static void encontrar(String palabraBuscada, String direccionDelArchivo, Condicion condicion) throws IOException {
+	private static void encontrar(String palabraBuscada, String direccionDelArchivo, Condicion condicion) {
 		try (Stream<String> stream = Files.lines(Paths.get(direccionDelArchivo))) {
 			if(stream.anyMatch(line -> condicion.comparacion(palabraBuscada,line))) {
 				throw new PasswordInseguraException("La contaseña ingresada es insegura");
 			}
 			
 		} catch(IOException e) {
-			System.out.println(e.getMessage());
+			throw new RuntimeException(e);
 		}
 	}
 	
