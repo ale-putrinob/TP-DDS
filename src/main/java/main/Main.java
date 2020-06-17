49bed68f6322f6d3f93d9437dc9506cd05262098
okitavio <okitavio@hotmail.com>
2020-06-17 16:40:03 -0300

    Planteo inicial de metodo main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import dominio.validacionEgresos.ValidadorEgresos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ValidadorEgresos validador = ValidadorEgresos.getInstance();
+		
+		//validar todos los ingresos pendientes de una organizacion
+		
+	}
+
+}
