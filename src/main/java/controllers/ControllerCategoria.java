5c71e6ff133f5a5212e08b39793560c340967398
AgustinCardozo <the_agustin@hotmail.com>
2020-11-11 16:24:52 -0300

    Modifico controllerCategoria

--- src/main/java/controllers/ControllerCategoria.java
+++ src/main/java/controllers/ControllerCategoria.java
@@ -1,13 +1,39 @@
 package controllers;
 
+import java.math.BigDecimal;
+
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+//import dominio.categoriaEntidad.BloquearNuevasEntidadesBase;
+import dominio.categoriaEntidad.CategoriaEntidad;
+import dominio.categoriaEntidad.RepoCategoria;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-public class ControllerCategoria {
-	public static ModelAndView getCategoria(Request req, Response res) {
+public class ControllerCategoria implements WithGlobalEntityManager, TransactionalOps{
+	public static ModelAndView getCategoria(Request req, Response res) {	
 		return new ModelAndView(null, "cargarCategoria.hbs");
 	}
+	public Void cargarCategoria(Request req, Response res) {
+		String nombre = req.queryParams("nombre");
+		//String bloqueo1 = req.queryParams("bloqueo1"); 
+		//String bloqueo2 = req.queryParams("bloqueo2"); 
+		//String bloqueo3 = req.queryParams("bloqueo3"); 
+		
+		CategoriaEntidad categoria = new CategoriaEntidad(nombre);
+		
+		withTransaction(() ->{
+			RepoCategoria.INSTANCE.agregarCategoria(categoria);;
+		});
+		
+		res.redirect("/categoria");
+		return null;
+	}
+	
 	public static ModelAndView asociarConEntidad(Request req, Response res) {
 		return new ModelAndView(null, "asociarCategoriaConEntidad.hbs");
 	}
